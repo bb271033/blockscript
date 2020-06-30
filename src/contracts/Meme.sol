dc6981a619eac583056b21c3a869a6f3c862be3a
bb271033 <nisakorn.buriram@gmail.com>
2020-06-30 15:57:10 +0700

    config truffle testnet

---
+++ src/contracts/Meme.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+contract Meme {
+   string memeHash;
+
+   // Write Function
+   function set(string memory _memeHash) public {
+      memeHash = _memeHash;
+   }
+   // Read Function
+   function get() public view returns (string memory) {
+      return memeHash;
+   }
+}
---
+++ truffle-config.js
@@ -0,0 +1,34 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    rinkeby: {
+      provider: function () {
+        new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${process.env.infuraKey}`)
+      },
+      gasPrice: 21000000000,
+      network_id: 4
+    },
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
